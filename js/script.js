a149836370828a9a0e94ca5738ac575c121325ca
Barbarubia <dnl.desimone@gmail.com>
2022-03-04 10:31:01 -0300

    iniziazione progetto

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*
+Esercizio di oggi: Simon Says
+nome repo: js-simon
+Descrizione:
+Visualizzare in pagina 5 numeri casuali.
+Da lì parte un timer di 30 secondi. (Cioè lasciate i numeri visibili per 30 secondi allo scadere dei quali li nascondete)
+Dopo aver nascosto i numeri chiedete all'utente (con dei prompt) di inserirli in ordine, uno alla volta.
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+Consigli del giorno:
+* Pensate prima in italiano.
+* Dividete in piccoli problemi la consegna.
+* Individuate gli elementi di cui avete bisogno per realizzare il programma.
+*/
